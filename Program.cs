54cccc6ab108bdd923e9209fc8d840149fe9e747
Melissa S Ruiz <melissa.salinas.ruiz@gmail.com>
2025-09-10 15:26:14 -0300

    Tabela de Multiplicacao em C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class TabelaMultiplicacao
+{
+    
+    public static void Multiplicar(int numero)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+        }
+    }
+    static void Main(string[] args)
+    {
+        int seuNumero;
+        Console.WriteLine("Digite um número:");
+        seuNumero = Convert.ToInt32(Console.ReadLine());
+
+        Multiplicar(seuNumero);
+
+    }
+}
